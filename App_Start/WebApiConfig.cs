8b891a2d523a3e670bf4e3a14d7b595d074d1a75
chagane <chagane.hamza@etu.uae.ac.ma>
2024-06-06 13:05:08 +0100

    table

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -5,10 +5,10 @@ public static class WebApiConfig
 {
     public static void Register(HttpConfiguration config)
     {
-        // Configuration et services API Web
+        // Configuration et services API Webgit 
 
         // Enable CORS
-        var cors = new EnableCorsAttribute("*", "*", "*"); // Allow all origins, headers, and methods
+        var cors = new EnableCorsAttribute("https://localhost:44330", "https://localhost:3000", "*"); // Allow all origins, headers, and methods
         config.EnableCors(cors);
 
         // Configuration des routes de l'API Web
